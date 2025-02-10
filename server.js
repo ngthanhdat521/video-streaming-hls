570a09d5d4b37d6551c582fe9366eecc2206dae2
ngthanhdat521 <84183823+ngthanhdat521@users.noreply.github.com>
2025-02-10 22:50:55 +0700

    update

--- server.js
+++ server.js
@@ -34,12 +34,6 @@ app.post('/upload', (req, res) => {
     `ffmpeg -i ${inputFilePath} -c:v libx264 -c:a aac -strict experimental -f hls -hls_time 10 -hls_list_size 0 -hls_segment_filename hls/segment_%03d.ts hls/output.m3u8`,
     // ffmpeg -i ${inputFilePath} -c:v libx264 -c:a aac -strict experimental -f hls -hls_time 10 -hls_list_size 0 -hls_segment_filename "video/segment_%03d.ts" "video/output.m3u8"
     (error, stdout, stderr) => {
-      // let output = fs.readFileSync('hls/output.m3u8').toString();
-
-      // output = output.replace('#EXT-X-ENDLIST\n', '');
-
-      // fs.writeFileSync('hls/output.m3u8', output);
-
       // Xoá tệp đã tải lên
       if (error) {
         console.error(`exec error: ${error}`);
